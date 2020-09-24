66253b404c02d6052703518fa5b858cfe562bb04
sameasychen <45924533+sameasychen@users.noreply.github.com>
2020-09-24 17:44:01 -0400

    Add sum-number js

---
+++ code questions/sumnum.js
@@ -0,0 +1,26 @@
+// Your company assigns each customer a membership ID, and you are implementing a check digit for those IDs. 
+// The check digit should be calculated by adding up all digits in each membership ID. If the result of the sum is a number with more than a single digit, another iteration is required, and the digits of the result also should be added together. This process should repeat until a single-digit number is calculated. For example, for the membership ID "55555" the sum of all digits is 25. Because this is not a single-digit number, 2 and 5 would be added, and the result 7, would be the check digit. 
+
+
+function createCheckDigit(membershipId) {
+
+    var sum = membershipId
+    while (sum > 10) {
+        let numDit = sum.toString().length
+        let left = sum
+        let calSum=0
+        
+        for (i = 0; i <= numDit; i++) {
+
+            var lastDig = left % 10;
+            calSum += lastDig
+            left = (left - lastDig) / 10
+        }
+        sum = calSum
+        
+    }
+
+    return sum;
+}
+
+console.log(createCheckDigit(55551));
