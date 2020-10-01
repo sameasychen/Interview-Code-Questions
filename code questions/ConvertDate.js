86c635f84e1bd4f38c5b02f1c3ef4314bf22b9a4
sameasychen <45924533+sameasychen@users.noreply.github.com>
2020-10-01 09:17:43 -0400

    Add ConcertDate

---
+++ code questions/ConvertDate.js
@@ -0,0 +1,19 @@
+// Write a function that converts user entered date formatted as M/D/YYYY to a format required by an API (YYYYMMDD). The parameter "userDate" and the return value are strings. For example, it should convert user entered date "12/31/2014" to "20141231" suitable for the API. 
+
+function formatDate(userDate) {
+    // format from M/D/YYYY to YYYYMMDD
+    var theDate = new Date(userDate)
+    let y = theDate.getFullYear().toString()
+    let m = (theDate.getMonth()+1).toString()
+    let d = theDate.getDate().toString()
+
+    var parts= userDate.split('/')
+    let yy = parts[2].toString()
+    let mm = parts[0].toString()
+    let dd = parts[1].toString()
+
+
+    return y+m+d
+  }
+  
+  console.log(formatDate("12/30/2014"));
